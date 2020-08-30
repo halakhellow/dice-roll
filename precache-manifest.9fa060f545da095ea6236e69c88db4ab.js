b4feba82eef93360ecfb6ffff4d003941cf663b8
Hala Alkhellow <halakhellow1996@gmail.com>
2020-08-31 00:23:42 +0300

    Updates

---
+++ precache-manifest.9fa060f545da095ea6236e69c88db4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0385f57c682a5e7ecb11ff28fa514d0",
+    "url": "/dice-roll/index.html"
+  },
+  {
+    "revision": "16f8c60278ddfe80f4b0",
+    "url": "/dice-roll/static/css/main.634739ec.chunk.css"
+  },
+  {
+    "revision": "944b52c28e42565d7033",
+    "url": "/dice-roll/static/js/2.98856e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roll/static/js/2.98856e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f8c60278ddfe80f4b0",
+    "url": "/dice-roll/static/js/main.f66bacaf.chunk.js"
+  },
+  {
+    "revision": "373c2847e37adb01b6cb",
+    "url": "/dice-roll/static/js/runtime-main.d172bf85.js"
+  }
+]);
